0bcfd5da4fb7443119925809fe2df83925ee4b13
Jose-Ovando99 <162391184+Jose-Ovando99@users.noreply.github.com>
2024-12-19 18:00:44 -0600

    finalizar proyecto

--- src/App.tsx
+++ src/App.tsx
@@ -4,15 +4,58 @@ import { LoadingButton } from "@mui/lab";
 import { Box, Container, TextField, Typography } from "@mui/material";
 import { useState } from "react";
 
-export default function App() {
+const API_WEATHER = `http://api.weatherapi.com/v1/current.json?key=${import.meta.env.VITE_API_KEY}&q=`
 
-  const onSubmit = (e: React.FormEvent<HTMLFormElement>) => {
-    e.preventDefault();
-    console.log(e);
-  }
+export default function App() {
 
   const [city, setCity] = useState<string>("")
   const [loading, setLoading] = useState<boolean>(false)
+  const [error, setError] = useState({
+    error: false,
+    message: "",
+  });
+  const [weather, setWeather] = useState({
+    city: "",
+    country: "",
+    temp: "",
+    condition: "",
+    icon: "",
+    conditionText: "",
+  });
+
+  const onSubmit = async(e: React.FormEvent<HTMLFormElement>) => {
+    e.preventDefault();
+    setLoading(true);
+    setError({
+      error: false,
+      message: "",
+    })
+    try {
+      if(!city.trim()) throw { message: "El campo ciudad es obligatorio" };
+      const response = await fetch(`${API_WEATHER}${city}`);
+      const data = await response.json();
+
+      if (data.error) throw { message: data.error.message };
+      
+      setWeather({
+        city: data.location.name,
+        country: data.location.country,
+        temp: data.current.temp_c,
+        condition: data.current.condition.code,
+        icon: data.current.condition.icon,
+        conditionText: data.current.condition.text,
+      })
+
+    } catch (error: any) {
+      console.log(error);
+      setError({
+        error: true,
+        message: error.mesagge,
+      });
+    } finally {
+      setLoading(false);
+    }
+  };
 
   return (
     <Container 
@@ -39,6 +82,8 @@ export default function App() {
         fullWidth
         value={city}
         onChange={(e) => setCity(e.target.value)}
+        error={error.error}
+        helperText={error.message}
         />
 
         <LoadingButton
@@ -50,6 +95,34 @@ export default function App() {
           Buscar
         </LoadingButton>
       </Box>
+
+      {weather.city && (
+        <Box
+        sx={{
+          mt: 2,
+          display: "grid",
+          gap: 2,
+          textAlign: "center",
+        }}
+        >
+          <Typography variant="h4" component="h2">
+            {weather.city}, {weather.country}
+          </Typography>
+          <Box
+            component="img"
+            alt={weather.conditionText}
+            src={weather.icon}
+            sx={{ margin: "0 auto" }}
+            />
+          <Typography variant="h5" component="h3">
+            {weather.temp} °C
+          </Typography>
+          <Typography variant="h6" component="h4">
+            {weather.conditionText}
+          </Typography>
+          
+        </Box>
+      )}
 
       <Typography
         textAlign="center"
